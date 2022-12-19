b2d39045b555f901c6409953dee9a69d7599d3d6
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2022-12-19 12:26:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+  $(function(){
+$(document).ready(function() {
+  $('.aboutme__more-arrow').click(function(event){
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
+}); 
+});
+//------------------------------------------------------------------------Спойлер
+
+
+
